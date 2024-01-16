39659a508f49205dafd58a20a12e0ff9ecbece07
jamshidakramovich14 <jamshidturdiyev103>
2024-01-16 07:55:29 +0500

    all files

---
+++ script.js
@@ -0,0 +1,80 @@
+// 4-DARS
+
+// 1- masala
+
+// let a = 0;
+// let b = 11;
+// let count = 0;
+// for(let i = a; i <= b; i++){
+//     console.log(i)
+//     count += 1;
+// }
+
+// console.log(count);
+
+// 2- masala
+
+// let a = 1;
+// let b = 10;
+// let count = 0;
+// for(let i = b-1; i > a && i < b;i--){
+//     console.log(i)
+//     count += 1;
+// }
+
+// console.log(count);
+
+// 3- masala
+
+// let narx = 1000;
+// for(let i = 1; i <= 10; i++){
+//     console.log(i.toFixed(1)*narx);
+// }
+
+// 4- masala
+
+// let narx = 1000;
+// for (let i = 1; i <= 2; i += 0.2) {
+//   console.log(i.toFixed(1) * narx);
+// }
+
+//  5- masala
+
+// let a = 1;
+// let b = 10;
+// let count =0;
+// for(let i = a; i <= b; i++){
+//     count += i;
+// }
+// console.log(count);
+
+// 6- masala
+
+// let a = 1;
+// let b = 10;
+// let count = a ;
+// for(let  i = a; i <= b; i++){
+//     count = count * i;
+// }
+
+// console.log(count);
+
+// 7- masala
+
+// let a = 1;
+// let b = 10;
+// let count = 0;
+// for(let i = a; i <= b; i++){
+//     count = count + (i*i);
+// }
+// console.log(count)
+
+//  9 - masala
+
+// let n = 5;
+// let s = 0 ;
+
+// for(let i = 1; i <= n; i + 0.1){
+//     s = s + i;
+// }
+// console.log(n);
